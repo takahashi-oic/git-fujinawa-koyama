aefa46f607644a15f42b52b92c3c2b9bf31971e0
16110013 <16110013@14dell-59.oic_lan1>
2018-11-22 14:34:04 +0900

    new 新規作成
    (cherry picked from commit 8ee5c1ffb1f7798c5082a27ed7897524c57854a2)

---
+++ api/errors/404.php
@@ -0,0 +1,18 @@
+<?php http_response_code(404); ?>
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>アンケート収集公開API 404ページ</title>
+	</head>
+
+	<body>
+		<h1>404 Not FOUND!</h1>
+		<p>お探しのURLは存在しませんでした。</p>
+		<a href="http://127.0.0.1/git-fujinawa-koyama/index.php">アンケート収集公開APIトップページ</a>
+		<br>
+		<a href="http://127.0.0.1/git-fujinawa-koyama/apiMain.php">アンケートAPI-API概要</a>
+	</body>
+</html>
