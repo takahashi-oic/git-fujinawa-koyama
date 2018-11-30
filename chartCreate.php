54f0daae45ce465e6337abf9f36d1c235079de01
16110013 <16110013@14DELL-59.OIC_LAN1>
2018-11-30 10:59:06 +0900

    chartCreate 更新 高橋

--- chartCreate.php
+++ chartCreate.php
@@ -3,7 +3,8 @@
             //DB接続
             //host localhost,192.168.201.xxxなど 106koya 99 ju
             $dsn = 'mysql:dbname=questionnaire_db;host=192.168.201.99;charset=utf8mb4';//mysql
-            $username = 'user';//ユーザー root user
+            //$username = 'root';//ユーザー root
+            $username = 'user';//ユーザー user
             $password = '';//パス
             $pdo = new PDO($dsn, $username, $password);
             
@@ -11,19 +12,19 @@
             $stmt = $pdo->prepare("select result from questionnaire_result");
             $stmt->execute();
             
-            $resArray = array();
+            $workArray = array();
             //結果取得
             while($row = $stmt -> fetch()){
-                $resArray[] = $row['result'];
+                $workArray[] = $row['result'];
             }
             //結果の数を数える
-            $valCount = array_count_values($resArray);
-            /*foreach($valCount as $key => $value){
-                
-            }*/
-            print_r($resArray);
-            echo("<br>");
-            print_r($valCount);
+            $resultArray = array_count_values($workArray);
+            arsort($resultArray);
+            //test
+            echo("work<br>");
+            print_r($workArray);
+            echo("<br>count<br>");
+            print_r($resultArray);
             
             //ここまで処理
         } catch (PDOException $e) {
@@ -65,36 +66,43 @@
         <p>取ってきた値から棒グラフ、円グラフなどのグラフを表示させる</p>
         <input type ="button" value="戻る" id="back" onclick="location.href='user.php'">
         <input type ="button" value="グラフ作成" id="chart">
-        <input type ="button" value="jsonてすと" id="jtest">
         
         <!--<canvas id="myChart"></canvas>-->
 
         <canvas id="jsontest" />
         
         <script>
-            var json = `[
-		{ "label": "赤池町",  "data": "1195"},
-    		{ "label": "浅田町",  "data": "1899"},
-    		{ "label": "梅森町",  "data": "804"},
-    		{ "label": "野方町",  "data": "133"},
-   		{ "label": "蟹甲町", "data": "158"}
-            ]`;
-            //オブジェクトに変換
-            obj = JSON.parse(json);
+            //phpからJSON key=場所val=数
+            var json = <?php echo json_encode($resultArray); ?>
+            
             //配列に変換
-            var data = new Array(obj.length);
-            var labels = new Array(obj.length);
-            for(var i=0; i<obj.length; i++){
-                data[i] = obj[i].data;
-                labels[i] = obj[i].label;
-            }
+            var data = new Array(json.length);
+            var labels = new Array(json.length);
+            //参考
+            //https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/values
+            
+            data = Object.values(json);
+            labels = Object.keys(json);
+            
+            //test
+            console.log(json);
+            console.log(data);
+            console.log(labels);
             
             var config = {
                 type: 'pie',
                 data:{
                     datasets:[{
                             data: data,
-                            backgroundColor: "rgba(153,255,51,1)"
+                            backgroundColor: [
+                                "#2ecc71",
+                                "#3498db",
+                                "#95a5a6",
+                                "#9b59b6",
+                                "#f1c40f",
+                                "#e74c3c",
+                                "#34495e"
+                                ]
                     }],
                     labels: labels
                 },
@@ -103,36 +111,11 @@
                     maintainAspectRatio: false
                 }
             };
-        document.getElementById("jtest").onclick = function() {
+        document.getElementById("chart").onclick = function() {
             var ctx = document.getElementById("jsontest").getContext('2d');
             ctx.canvas.height = 280;//グラフの高さ
             var myChart = new Chart(ctx, config);
         };
-        
-
-        /*document.getElementById("chart").onclick = function() {
-            var ctx = document.getElementById("myChart").getContext('2d');
-            ctx.canvas.height = 280;//グラフの高さ
-            var myChart = new Chart(ctx, {
-              type: 'bar',
-              data: {
-                labels: ["M", "T", "W", "R", "F", "S", "S"],//JSON 日付など
-                datasets: [{
-                  label: 'apples',//データ名
-                  data: [12, 19, 3, 17, 28, 24, 7],//JSON データ
-                  backgroundColor: "rgba(153,255,51,1)"
-                }, {
-                  label: 'oranges',
-                  data: [30, 29, 5, 5, 20, 3, 10],
-                  backgroundColor: "rgba(255,153,0,1)"
-                }]
-              },
-              options: {
-              responsive: true,//グラフの横幅
-              maintainAspectRatio: false
-            }
-            });
-        }*/
         </script>
     </body>
 </html>
