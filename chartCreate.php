3aaa81f8abfee0a95cf8579ef2baaa9ed399a9d6
takahashi <takahashi@14DELL-59.OIC_LAN1>
2018-12-03 14:36:07 +0900

    chartCreate update 高橋

--- chartCreate.php
+++ chartCreate.php
@@ -46,7 +46,7 @@
         <!-- Compiled and minified CSS -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
         <!-- Compiled and minified JavaScript -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
         
         <title>グラフ作成ページ</title>
         
