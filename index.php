4d8d35160e54cc0af5bb59428f4ec5aabfb99a50
koyama <kym1290kh@gmail.com>
2018-12-03 11:31:17 +0900

    commit 小山

--- index.php
+++ index.php
@@ -70,11 +70,11 @@ and open the template in the editor.
         </div>
     </body>
     <footer class="page-footer">
-          <div class="footer-copyright">
+        <div class="footer-copyright">
             <div class="container">
-            © 2014 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+                © 2014 Copyright Text
+                <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
             </div>
-          </div>
-        </footer>
+        </div>
+    </footer>
 </html>
