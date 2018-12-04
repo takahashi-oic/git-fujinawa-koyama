15a0fe36864399e087b5a327b40650010b29441b
HAL0419 <h.ishisaka.0419@gmail.com>
2018-12-04 10:18:12 +0900

    update -> directory[api] update -> file[Moredetail.php] * 404ページの追加・遷移 * API詳細仕様の改良

--- Moredetail.php
+++ Moredetail.php
@@ -1,6 +1,17 @@
 <?php
-
+	switch(rand(0, 2)) {
+		case 0:
+			$_GET['format'] = 'json';
+			break;
+		case 1:
+			$_GET['format'] = 'xml';
+			break;
+		case 2:
+			$_GET['format'] = 'csv';
+			break;
+	}
 ?>
+
 <!DOCTYPE html>
 
 <html lang="ja">
@@ -13,41 +24,33 @@
 
 		<!-- Compiled and minified JavaScript -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-		<style type="text/css">
 
-		</style>
+		<!-- 使用例の外部CSS読込みJavaScript -->
+		<script type="text/javascript" src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
+		<style type="text/css"></style>
+
 		<title>アンケートAPI-API詳細仕様</title>
 	</head>
-	<!--<header>
+
+	<!--
+	<header>
 			<div class="card-panel row s12 light-green lighten-1"><span class="white-text">API仕様書</span></div>
 			<div class="col s12">
 				<ul class="tabs">
 					<li class="tab col s3" id="home"><a href="index.php"><span class="light-green-text text-lighten-1">ホーム</span></a></li>
 				</ul>
 			</div>
-	</header>-->
+	</header>
+	-->
+
 	<body>
-		<ul id="slide-out" class="sidenav sidenav-fixed">
-			<div class="row">
-				<a href="index.php">
-					<i class="medium material-icons left black-text" id="homebtn">home</i>
-				</a>
-			</div>
-			<div>
-				<p>　はじめに</p>
-				<li><a href="apiMain.php" class="blue-text text-darken-2">API概要</a></li>
-				<li><a href="Moredetail.php" class="blue-text text-darken-2">API詳細仕様</a></li>
-				<p>　API一覧</p>
-				<li><a href="#!" class="blue-text text-darken-2">性別一覧</a></li>
-				<li><a href="#!" class="blue-text text-darken-2">国一覧</a></li>
-				<li><a href="#!" class="blue-text text-darken-2">購入したもの一覧</a></li>
-			</div>
-		</ul>
+		<?php include_once('api/src/nav.php'); ?>
 		<div class="container">
 			<div class="row">
 				<div class="col s10 offset-s2">
 					<h2>API詳細仕様</h2>
 					<br>
+
 					<h4 class="truncate">利用方法</h4>
 					<div class="row">
 						<p>
@@ -81,8 +84,17 @@
 					</div>
 
 					<h4 class="truncate">使用例</h4>
-					<?php $_GET['format'] = 'json'; ?>
-					<pre><?php include_once('./api/database.php') ?></pre>
+					<div>
+						<div>
+							<p>HTTPリクエスト</p>
+							<pre class="prettyprint">GET http://localhost/git-fujinawa-koyama/api/database.php?format=<?= $_GET['format'] ?></pre>
+						</div>
+
+						<div>
+							<p>HTTPレスポンス</p>
+							<pre class="prettyprint"><?php include_once('./api/database.php') ?></pre>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
--- api/src/ContentType.php
+++ api/src/ContentType.php
@@ -1,31 +1,8 @@
 <?php
-	abstract class Enum {
-		private $value;
-
-		public function __construct($value) {
-			$ref = new ReflectionObject($this);
-			$const = $ref->getConstants();
-
-			if(!in_array($value, $const, true)) {
-				throw new InvalidArgumentException;
-			}
-
-			$this->value = $value;
-		}
-
-		final public static function __callStatic($label, $args) {
-			$class = get_called_class();
-			$const = constant("$class::$label");
-			return new $class($const);
-		}
-
-		final public function __toString() {
-			return (string)$this->value;
-		}
-	}
+	include_once "Enumeration.php";
 
 	class ContentType
-		extends Enum {
+		extends Enumeration {
 		const JSON = "application/json";
 		const XML = "application/xml";
 		const CSV = "text/csv";
