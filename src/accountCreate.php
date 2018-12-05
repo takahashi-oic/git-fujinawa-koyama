c3b5d02ba01e99ecb94659dcbe2e8dc8d5f15d6b
takahashi <16110013@14DELL-59.OIC_LAN1>
2018-12-05 13:34:30 +0900

    accountCreate update 高橋

--- src/accountCreate.php
+++ src/accountCreate.php
@@ -13,16 +13,16 @@
         <title>アカウント作成</title>
     </head>
     <body>
-    <header>
-        <div class="card-panel row s12 light-green lighten-1"><span class="white-text">管理者用アカウント作成</span></div>
-        <div class="col s12">
-            <ul class="tabs">
-                <li class="tab col s3" id="home"><a href="index.php"><span class="light-green-text text-lighten-1">ホーム</span></a></li>
-                <li class="tab col s3" id="home"><a href=""><span class="light-green-text text-lighten-1">Name2</span></a></li>
-                <li class="tab col s3" id="api"><a href="apiMain.php"><span class="light-green-text text-lighten-1">API仕様書</span></a></li>
-            </ul>
-        </div>
-    </header>
+        <header>
+            <div class="card-panel row s12 light-green lighten-1"><span class="white-text">管理者用アカウント作成</span></div>
+            <div class="col s12">
+                <ul class="tabs">
+                    <li class="tab col s3" id="home"><a href="index.php"><span class="light-green-text text-lighten-1">ホーム</span></a></li>
+                    <li class="tab col s3" id="home"><a href=""><span class="light-green-text text-lighten-1">Name2</span></a></li>
+                    <li class="tab col s3" id="api"><a href="apiMain.php"><span class="light-green-text text-lighten-1">API仕様書</span></a></li>
+                </ul>
+            </div>
+        </header>
 
         <br><br><br>
         <form action="accountConfirm.php" method="post">
@@ -39,7 +39,8 @@
                         </div>
                     </div>
                     <div class="row">
-                        <input type="submit" value="確認ページへ" class="waves-effect waves-light btn col s4 offset-s4" id="createbtn">
+                        <!--<input type="submit" value="確認ページへ" class="waves-effect waves-light btn col s4 offset-s4" id="createbtn">-->
+                        <button type="submit" name="action" class="btn waves-effect waves-light col s4 offset-s4" id="createbtn">確認ページへ</button>
                     </div>
                 </div>
             </div>
