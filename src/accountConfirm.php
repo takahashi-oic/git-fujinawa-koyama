72a90133d1b186353fd4e3b78ef0910a82403486
kaito555 <jackkaito555@gmail.com>
2018-12-05 11:28:54 +0900

    見て　重里

--- src/accountConfirm.php
+++ src/accountConfirm.php
@@ -8,6 +8,19 @@
 
         <!-- Compiled and minified JavaScript -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <?php
+           try{
+    $pdo = new PDO('mysql:host=localhost;dbname=questionnaire_db;charset=utf8','test','test');
+    
+ 
+  } catch (PDOException $e) {
+    header('Content-Type: text/plain; charset=UTF-8', true, 500);
+    exit($e->getMessage()); 
+  }
+            //$_POSTで受け取る
+            $userid = $_POST['userid'];
+            $password = $_POST['password'];
+        ?>
         <title>アカウント確認</title>
     </head>
     <header>
@@ -21,10 +34,42 @@
         </div>
     </header>
     <body>
-        <div class="row">
-            <a href="adminLogin.php" class="waves-effect waves-light btn col s8 offset-s2" id="okBtn">送信</a>
-        </div>
+        <form method="post">
+              <div class="row">
+                    <div class="col s4 offset-s4 z-depth-3">
+                         <div class="row">
+                                 <div class="input-field col s9 offset-s1">
+
+                                     <input id="userid" name="setuserid" type="text" class="validate">
+                                 </div>
+                                 <div class="input-field col s9 offset-s1">
+
+                                     <input id="password" name="setpassword" type="text" class="validate">
+                                 </div>
+                             </div>
+                             <div class="row">
+                     </div>
+                        <input type="submit" class="waves-effect waves-light btn col s8 offset-s2" id="okBtn" value="送信" onSubmit="jump()">
+                        <script>
+
+                                function jump(){
+                                   //TOPページにジャンプさせる
+                                   location.href = 'http://localhost/git-fujinawa-koyama/src/index.php';
+                                }
+
+                        </script>
+                        <?php
+                         if(true == isset($_POST['setuserid'])&&true == isset($_POST['setpassword'])){ 
+                         //$_POSTで受け取る
+                         $setuserid = $_POST['setuserid'];
+                         $setpassword = $_POST['setpassword'];
+                         $pdo->exec("INSERT INTO account(user_id,password) "."VALUES ('$setuserid', '$setpassword')");
+                         }
+                     ?>
+                 </div>
+           </div>
+        </form>
+                    
+       
     </body>
 </html>
-
-
--- src/accountCreate.php
+++ src/accountCreate.php
@@ -10,12 +10,6 @@
 
         <!-- Compiled and minified JavaScript -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-        <style type="text/css">
-            #createbtn{
-                color:white;
-                outline: none;
-            }
-        </style>
         <title>アカウント作成</title>
     </head>
     <body>
@@ -29,27 +23,26 @@
             </ul>
         </div>
     </header>
+
         <br><br><br>
         <form action="accountConfirm.php" method="post">
             <div class="row">
-                <div class="col s4 offset-s4 z-depth-2">
+                <div class="col s4 offset-s4 z-depth-3">
                     <div class="row">
                         <div class="input-field col s9 offset-s1">
                             <i class="material-icons prefix">account_box</i>
-                            <input id="userid" type="text" class="validate" placeholder="ユーザーID（半角英数字12文字以内）"required>
+                            <input id="userid" name="userid" type="text" class="validate" required="required" placeholder="ユーザーID（半角英数字12文字以内）">
                         </div>
                         <div class="input-field col s9 offset-s1">
                             <i class="material-icons prefix">lock</i>
-                            <input id="password" type="text" class="validate" placeholder="password（半角英数字8～20文字以内)" required>
+                            <input id="password" name="password" type="text" class="validate" required="required" placeholder="password（半角英数字8～20文字以内)">
                         </div>
                     </div>
                     <div class="row">
-                        <input type="submit" class="waves-effect waves-light btn col s4 offset-s4" id="createbtn" value="確認ページへ">
+                        <input type="submit" value="確認ページへ" class="waves-effect waves-light btn col s4 offset-s4" id="createbtn">
                     </div>
                 </div>
             </div>
         </form>
     </body>
 </html>
-
-
