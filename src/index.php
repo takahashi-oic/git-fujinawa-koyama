cf591a473d0e49134708c238c67b1c9ef72dcde0
koyama <kym1290kh@gmail.com>
2018-12-10 11:31:04 +0900

    commit 小山

--- src/index.php
+++ src/index.php
@@ -9,7 +9,7 @@
         <!-- Compiled and minified JavaScript -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
         <script type="text/javascript"></script>
-        <title>アンケート収集公開APIページ</title>
+        <title>アンケート収集公開APIトップページ</title>
         <!-- CSS -->
         <style type="text/css">
             .col {/*親div*/
