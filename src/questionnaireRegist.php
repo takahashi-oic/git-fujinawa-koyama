f0d0c63a1fb3cffbf57f497ad898e9579c4815a4
kaito555 <jackkaito555@gmail.com>
2019-01-08 14:50:07 +0900

    アンケート 変更　重里

--- src/questionnaireRegist.php
+++ src/questionnaireRegist.php
@@ -7,6 +7,6 @@
                 header('Content-Type: text/plain; charset=UTF-8', true, 500);
                 exit($e->getMessage());
             }
-            require "createPreview.php";
+            require_once "createPreview.php";
             echo $ques;
              ?>
