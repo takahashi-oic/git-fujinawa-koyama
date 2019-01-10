7a17610312b198e6288dd308731b99ef45a50b53
HAL0419 <h.ishisaka.0419@gmail.com>
2019-01-10 15:59:15 +0900

    hotfix

--- api/questionnaire.php
+++ api/questionnaire.php
@@ -7,4 +7,18 @@
 	$result = $query->query(1);
 
 	$fmt = new src\api\formatter\JsonFormat();
+
+	if(key_exists('format', $_GET)) switch(strtolower($_GET['format'])) {
+		case 'json':
+			$fmt = new src\api\formatter\JsonFormat();
+			break;
+
+		case 'xml':
+			$fmt = new src\api\formatter\XmlFormat();
+			break;
+
+		default:
+			http_err(403);
+			return;
+	}
 	echo $fmt->convert($result);
