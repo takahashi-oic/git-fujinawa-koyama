1a85bbd3340742fe62bf405ee5412e44596e7dd5
HAL0419 <h.ishisaka.0419@gmail.com>
2019-01-10 11:55:40 +0900

    hotfix -> File[nav.php]

--- api/questionnaire.php
+++ api/questionnaire.php
@@ -1,13 +1,9 @@
 <?php
 	declare(strict_types = 1);
 
-	/*
 	include_once('AutoLoader.php');
 
 	$query = new src\api\Question();
 	$result = $query->query(1);
 
 	while($column = $result->fetch()) echo $column;
-	*/
-
-	echo __DIR__;
