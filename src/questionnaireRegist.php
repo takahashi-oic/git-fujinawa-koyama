1d4424b607329ea167b94956c73a43037776cb0f
kaito555 <jackkaito555@gmail.com>
2019-01-10 10:00:50 +0900

    アンケート 変更　重里

--- src/questionnaireRegist.php
+++ src/questionnaireRegist.php
@@ -7,6 +7,11 @@
                 header('Content-Type: text/plain; charset=UTF-8', true, 500);
                 exit($e->getMessage());
             }
+            $date = new DateTime();
+            $dbdate = $date->format('Y-m-d H:i:s');
             $ques = $_POST['ques'];
-            echo $ques;
+            $stmt = $pdo->prepare("INSERT INTO questionnaire_info(url,conversion _data,create_date) "."VALUES ('$ques', '$ques','$dbdate')");
+            $stmt->execute();
+            
              ?>
+<input type="button" value="終了" onClick="location.href='adminTop.php'">
