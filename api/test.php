e6bbf96f468a39e0eb67247a71b1b718574f229e
HAL0419 <h.ishisaka.0419@gmail.com>
2019-01-11 16:50:10 +0900

    hotfix

--- api/test.php
+++ api/test.php
@@ -3,6 +3,8 @@
 
 	$url = parse_url(getenv('DATABASE_URL'));
 	$db = new PDO("pgsql:" . sprintf('host=%s;port=%s;user=%s;password=%s;dbname=%s', $url["host"], $url["port"], $url["user"], $url["pass"], ltrim($url["path"], "/")));
+
+	$db->
 	for($idx = 0; $idx < 10; $idx++) {
 		$rand = rand(1, 100);
 		$db->query("INSERT INTO age(age) VALUES (${rand})");
