a310133594ddf54744016e3de64cc2c225a160a6
HAL0419 <h.ishisaka.0419@gmail.com>
2019-01-11 10:05:16 +0900

    hotfix

--- api/questionnaire.php
+++ api/questionnaire.php
@@ -1,9 +1,35 @@
 <?php
 	declare(strict_types = 1);
 
-	include_once('AutoLoader.php');
+	/**
+	 * ## Class Question
+	 * @package database
+	 */
+	class Question {
+		/** ## SQLQuery */
+		private static $sql = "SELECT * FROM item WHERE question_num = :num";
+		private $db;
 
-	$query = new src\api\Question();
+		public function __construct() {
+			$url = parse_url(getenv('DATABASE_URL'));
+			$this->db = new PDO("pgsql:" . sprintf('host=%s;port=%s;user=%s;password=%s;dbname=%s', $url["host"], $url["port"], $url["user"], $url["pass"], ltrim($url["path"], "/")));
+		}
+
+		/**
+		 * ## SQL実行関数
+		 * @param int $num question_num
+		 * @return \PDOStatement SQL実行状態
+		 */
+		public function query(int $num) {
+			$query = $this->db->prepare(self::$sql);
+			$query->bindValue(':num', $num, PDO::PARAM_INT);
+
+			$query->execute();
+			return $query;
+		}
+	}
+
+	$query = new Question();
 	$data = $query->query(1);
 
 	$result = function() {
--- api/src/api/Question.php
+++ api/src/api/Question.php
@@ -2,33 +2,4 @@
 	declare(strict_types = 1);
 
 	namespace src\api {
-		use PDO;
-
-		/**
-		 * ## Class Question
-		 * @package database
-		 */
-		class Question {
-			/** ## SQLQuery */
-			private static $sql = "SELECT * FROM item WHERE question_num = :num";
-			private $db;
-
-			public function __construct() {
-				$url = parse_url(getenv('DATABASE_URL'));
-				$this->db = new PDO("pgsql:" . sprintf('host=%s;port=%s;user=%s;password=%s;dbname=%s', $url["host"], $url["port"], $url["user"], $url["pass"], ltrim($url["path"], "/")));
-			}
-
-			/**
-			 * ## SQL実行関数
-			 * @param int $num question_num
-			 * @return \PDOStatement SQL実行状態
-			 */
-			public function query(int $num) {
-				$query = $this->db->prepare(self::$sql);
-				$query->bindValue(':num', $num, PDO::PARAM_INT);
-
-				$query->execute();
-				return $query;
-			}
-		}
 	}
