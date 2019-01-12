3c5445b67071c4e888d3e0ca5b1309ff3aabe00f
HAL0419 <h.ishisaka.0419@gmail.com>
2019-01-12 14:34:05 +0900

    hotfix

--- api/src/api/Select.php
+++ api/src/api/Select.php
@@ -2,6 +2,7 @@
 	declare(strict_types = 1);
 
 	namespace src\api {
+		use Exception;
 		use PDO;
 		use PDOStatement;
 
@@ -17,9 +18,10 @@
 			 */
 			public function __construct() {
 				// $this->db = Database::getInstance()->connect();
-				$url = parse_url(getenv('DATABASE_URL'));
-				$this->db = new PDO("pgsql:" . sprintf('host=%s;port=%s;user=%s;password=%s;dbname=%s', $url["host"], $url["port"], $url["user"], $url["pass"], ltrim($url["path"], "/")));
-				if(!$this->db instanceof PDO) {
+				try {
+					$url = parse_url(getenv('DATABASE_URL'));
+					$this->db = new PDO("pgsql:" . sprintf('host=%s;port=%s;user=%s;password=%s;dbname=%s', $url["host"], $url["port"], $url["user"], $url["pass"], ltrim($url["path"], "/")));
+				} catch(Exception $e) {
 					http_response_code(400);
 					die(400);
 				}
