0c72d83bcf83e399f3a05e430da0d44ad3fc7f05
kaito555 <jackkaito555@gmail.com>
2019-01-15 05:29:15 +0900

    アンケート 変更　重里

--- src/questionnaireRegist.php
+++ src/questionnaireRegist.php
@@ -120,7 +120,7 @@
         
         <div class="row">
                         <!--<input type="submit" value="確認ページへ" class="waves-effect waves-light btn col s4 offset-s4" id="createbtn">-->
-            <button type="button" onClick="window.close()" class="btn waves-effect waves-light col s4 offset-s4" id="createbtn" >終了</button>
+            <button type="button" onClick="window.close();" class="btn waves-effect waves-light col s4 offset-s4" id="createbtn" >終了</button>
                     </div>
         
     </body>
