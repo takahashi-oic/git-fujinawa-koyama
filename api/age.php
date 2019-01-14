bea60144048951df8c5ced8faac0a8105bdaf6c9
HAL0419 <h.ishisaka.0419@gmail.com>
2019-01-15 00:34:27 +0900

    hotfix

--- api/age.php
+++ api/age.php
@@ -4,7 +4,7 @@
 	require_once('AutoLoader.php');
 
 	try {
-		$query = new src\api\Select('age');
+		$query = new Select('age');
 		//$data = $query->query();
 	} catch(Exception $e) {
 		echo $e->getMessage();
@@ -12,20 +12,20 @@
 
 	$result = null;
 
-	//	if(key_exists('format', $_GET)) {
-	//		$fmt = new src\api\Format();
-	//
-	//		switch(strtolower($_GET['format'])) {
-	//			case 'csv':
-	//				$result = $fmt->toCsv($data);
-	//				break;
-	//
-	//			case 'json':
-	//				$result = $fmt->toJson($data);
-	//				break;
-	//
-	//			case 'xml':
-	//				$result = $fmt->toXml($data);
-	//				break;
-	//		}
-	//	}
+	if(key_exists('format', $_GET)) {
+		$fmt = new src\api\Format();
+
+		switch(strtolower($_GET['format'])) {
+			case 'csv':
+				$result = $fmt->toCsv($data);
+				break;
+
+			case 'json':
+				$result = $fmt->toJson($data);
+				break;
+
+			case 'xml':
+				$result = $fmt->toXml($data);
+				break;
+		}
+	}
