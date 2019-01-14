e01a918e4bce991951a985348cf7e4a58e7d5bd2
HAL0419 <h.ishisaka.0419@gmail.com>
2019-01-14 20:29:42 +0900

    hotfix

--- api/age.php
+++ api/age.php
@@ -1,12 +1,13 @@
 <?php
 	declare(strict_types = 1);
 
+	require_once('AutoLoader.php');
+
 	$query = new src\api\Select('age');
 	$data = $query->query();
 
 	$result = null;
 
-	/*
 	if(key_exists('format', $_GET)) {
 		$fmt = new src\api\Format();
 
