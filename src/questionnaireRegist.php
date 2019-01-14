2e8c74e90e5d1cc1ab237dba04e3f0582fa512c9
kaito555 <jackkaito555@gmail.com>
2019-01-15 04:29:03 +0900

    アンケート 変更　重里

--- src/questionnaireRegist.php
+++ src/questionnaireRegist.php
@@ -23,11 +23,38 @@
             $Purchases3 = $_POST['Purchases3'];
             $purpose = $_POST['purpose'];
             $sns = $_POST['sns'];
+                        
+            $stmt1 = $pdo->prepare("INSERT INTO answer_info(ans_day) VALUES ('$now')");
+            $stmt2 = $pdo->prepare("INSERT INTO country(country) VALUES ('$country')");
+            $stmt3 = $pdo->prepare("INSERT INTO age(age) VALUES ('$age')");
+            $stmt4 = $pdo->prepare("INSERT INTO sex(sex) VALUES ('$sex')");
+            $stmt5 = $pdo->prepare("INSERT INTO inandout_airport(in_airport,out_airport) VALUES ('$inairport','$outairport')");
+            $stmt6 = $pdo->prepare("INSERT INTO tourism1(tourism1) VALUES ('$Tourism1')");
+            $stmt7 = $pdo->prepare("INSERT INTO tourism2(tourism2) VALUES ('$Tourism2')");
+            $stmt8 = $pdo->prepare("INSERT INTO tourism3(tourism3) VALUES ('$Tourism3')");
+            $stmt9 = $pdo->prepare("INSERT INTO tourism4(tourism4) VALUES ('$Tourism4')");
+            $stmt10 = $pdo->prepare("INSERT INTO tourism5(tourism5) VALUES ('$Tourism5')");
+            $stmt11 = $pdo->prepare("INSERT INTO purchases1(purchases1) VALUES ('$Purchases1')");
+            $stmt12 = $pdo->prepare("INSERT INTO purchases2(purchases2) VALUES ('$Purchases2')");
+            $stmt13 = $pdo->prepare("INSERT INTO purchases3(purchases3) VALUES ('$Purchases3')");
+            $stmt14 = $pdo->prepare("INSERT INTO purpose(purpose) VALUES ('$purpose')");
+            $stmt15 = $pdo->prepare("INSERT INTO sns(sns) VALUES ('$sns')");
             
-            echo $now;
-            
-            $stmt = $pdo->prepare("INSERT INTO answer_info(ans_day) VALUES ('$now')");
-            $stmt->execute();
+            $stmt1->execute();
+            $stmt2->execute();
+            $stmt3->execute();
+            $stmt4->execute();
+            $stmt5->execute();
+            $stmt6->execute();
+            $stmt7->execute();
+            $stmt8->execute();
+            $stmt9->execute();
+            $stmt10->execute();
+            $stmt11->execute();
+            $stmt12->execute();
+            $stmt13->execute();
+            $stmt14->execute();
+            $stmt15->execute();
             
             
                        
