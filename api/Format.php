2c2ca066fb3a514d94cfb0c3b41512e48677effd
HAL0419 <h.ishisaka.0419@gmail.com>
2019-01-16 14:37:34 +0900

    hotfix

--- api/Format.php
+++ api/Format.php
@@ -44,7 +44,7 @@
 		public function toJson(PDOStatement $database): string {
 			header('Content-type: application/json');
 
-			foreach($database->fetch(PDO::FETCH_ASSOC) as $key => $value) $this->data['result'] .= array($key => $value);
+			foreach($database->fetch() as $key => $value) $this->data['result'] .= array($key => $value);
 
 			// region JSON Setting
 			$opt = 0;
