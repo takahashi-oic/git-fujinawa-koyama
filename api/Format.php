5e1c454f154ea155ce31ec5a662c3b34a0e3fb27
HAL0419 <h.ishisaka.0419@gmail.com>
2019-01-17 10:59:14 +0900

    hotfix

--- api/Format.php
+++ api/Format.php
@@ -46,8 +46,7 @@
 
 			$idx = 0;
 			while($col = $database->fetch(PDO::FETCH_ASSOC)) {
-				foreach($col as $key => $value) $this->data['result'] += array(array($key => $value));
-				$idx++;
+				foreach($col as $key => $value) $this->data['result'] += array($idx++ => array($key => $value));
 			}
 
 			// region JSON Setting
