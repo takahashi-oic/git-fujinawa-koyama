0bb53c884fd3253d53fe1b67719b70a213bdada3
HAL0419 <h.ishisaka.0419@gmail.com>
2019-01-17 13:49:26 +0900

    hotfix

--- api/Format.php
+++ api/Format.php
@@ -76,17 +76,14 @@
 			header('Content-type: application/xml');
 
 			$header = '<?xml version="1.0" encoding="UTF-8" ?>';
-			$root = new SimpleXMLElement($header . '<api></api>');
+			$root = new SimpleXMLElement($header . '<api />');
 
 			// region XML Element
 			$msg = $root->addChild('msg', null);
 			$results = $root->addChild('results');
 
-			while($col = $database->fetch()) {
-				foreach($col as $key => $value) {
-					$results->addChild($col, $key);
-				}
-			}
+			while($col = $database->fetch()) foreach($col as $key => $value)
+				// $results->addChild($key, $value);
 			// endregion XML Element
 
 			// region XML Format
