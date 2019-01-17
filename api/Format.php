c5a9c686b57ae470f9ba2b229d61c675cd8a52fa
HAL0419 <h.ishisaka.0419@gmail.com>
2019-01-17 10:10:02 +0900

    hotfix

--- api/Format.php
+++ api/Format.php
@@ -45,7 +45,7 @@
 			header('Content-type: application/json');
 
 			$idx = 0;
-			while($col = $database->fetch()) {
+			while($col = $database->fetch(PDO::FETCH_COLUMN)) {
 				foreach($col as $key => $value) $this->data['result'] += array($idx => array($key => $value));
 				$idx++;
 			}
