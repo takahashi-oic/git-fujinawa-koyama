203249a7818c5a0852e4b4cb15f956bbe582328c
HAL0419 <h.ishisaka.0419@gmail.com>
2019-01-18 14:29:00 +0900

    hotfix

--- api/src/nav.php
+++ api/src/nav.php
@@ -42,19 +42,21 @@
 	 */
 	function home(string $root) {
 		session_start();
-		if(isset($_SESSION['userid'])) $home = 'admin';
-		else $home = 'index';
+		if(isset($_SESSION['userid'])) $home = 'admin'; else $home = 'index';
 		return $root . $home . '.php';
 	}
 
 	/**
 	 * ## APIリスト表示関数
-	 * @param \APIDispatcher ...$list 表示するAPIリスト
+	 * @param APIDispatcher ...$list 表示するAPIリスト
+	 * @return string listタグ
 	 */
-	function print_list(array $list) {
-		foreach($list as $elem) echo $elem;
-	}
+	function print_list(APIDispatcher... $list): string {
+		$value = '';
+		foreach($list as $elem) $value .= $elem;
 
+		return $value;
+	}
 ?>
 <ul id="slide-out" class="sidenav sidenav-fixed">
 	<div class="row">
@@ -65,29 +67,23 @@
 
 	<div>
 		<p>　はじめに</p>
+		<?= print_list(new APIDispatcher('apiMain.php', 'API概要'), new APIDispatcher('Moredetail.php', 'API詳細仕様')); ?>
 		<?php
-			print_list(array(
-				new APIDispatcher('apiMain.php', 'API概要'),
-				new APIDispatcher('Moredetail.php', 'API詳細仕様')
-			));
+			/* ## API仕様書
+			 * <li><a href="apiMain.php#" class="blue-text text-darken-2">API概要</a></li>
+			 * <li><a href="Moredetail.php#" class="blue-text text-darken-2">API詳細仕様</a></li>
+			 */
 		?>
-		<!--
-		<li><a href="apiMain.php#" class="blue-text text-darken-2">API概要</a></li>
-		<li><a href="Moredetail.php#" class="blue-text text-darken-2">API詳細仕様</a></li>
-		-->
 
 		<p>　API一覧</p>
+		<?= print_list(new APIDispatcher('Moredetail.php?send=database', '全集計結果'), new APIDispatcher('Moredetail.php?send=question', 'アンケートごとの結果'), new APIDispatcher('Moredetail.php?send=sex', '性別一覧'), new APIDispatcher('Moredetail.php?send=country', '国一覧'), new APIDispatcher('Moredetail.php?send=airport', '空港一覧'), new APIDispatcher('Moredetail.php?send=tourism', '観光地一覧'), new APIDispatcher('Moredetail.php?send=purchases', '購入したもの一覧')); ?>
 		<?php
-			print_list(array(
-				new APIDispatcher('Moredetail.php?send=database', '全集計結果'), new APIDispatcher('Moredetail.php?send=question', 'アンケートごとの結果'), new APIDispatcher('Moredetail.php?send=sex', '性別一覧'), new APIDispatcher('Moredetail.php?send=country', '国一覧'), new APIDispatcher('Moredetail.php?send=airport', '空港一覧'), new APIDispatcher('Moredetail.php?send=tourism', '観光地一覧'), new APIDispatcher('Moredetail.php?send=purchases', '購入したもの一覧'),
-			));
+			/* ## API取得可能データ一覧
+			 * <li><a href="#!" class="blue-text text-darken-2">性別一覧</a></li>
+			 * <li><a href="#!" class="blue-text text-darken-2">国一覧</a></li>
+			 * <li><a href="#!" class="blue-text text-darken-2">観光地一覧</a></li>
+			 * <li><a href="#!" class="blue-text text-darken-2">購入したもの一覧</a></li>
+			 */
 		?>
-
-		<!--
-		<li><a href="#!" class="blue-text text-darken-2">性別一覧</a></li>
-		<li><a href="#!" class="blue-text text-darken-2">国一覧</a></li>
-		<li><a href="#!" class="blue-text text-darken-2">観光地一覧</a></li>
-		<li><a href="#!" class="blue-text text-darken-2">購入したもの一覧</a></li>
-		-->
 	</div>
 </ul>
