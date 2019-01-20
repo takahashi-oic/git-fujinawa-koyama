83562db1b5541e8b08d8ebbdf801925d7f071c91
HAL0419 <h.ishisaka.0419@gmail.com>
2019-01-20 23:52:26 +0900

    hotfix

--- api/Format.php
+++ api/Format.php
@@ -86,7 +86,7 @@
 
 			while($col = $database->fetch()) {
 				$result = $results->addChild('result');
-				foreach($col as $key => $value) $result->addChild(strval($key), strval($value));;
+				foreach($col as $key => $value) $result->addChild($key);
 			}
 			// endregion XML Element
 
