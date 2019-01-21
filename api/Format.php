ceeb68f10679ab9c33f260457cfe0bd527879274
HAL0419 <h.ishisaka.0419@gmail.com>
2019-01-21 10:34:14 +0900

    hotfix

--- api/Format.php
+++ api/Format.php
@@ -86,7 +86,7 @@
 
 			while($col = $database->fetch(PDO::FETCH_ASSOC)) {
 				$result = $results->addChild('result');
-				foreach($col as $key => $value) $result->addChild(strval($key));
+				foreach($col as $key => $value) $result->addChild($key);
 			}
 			// endregion XML Element
 
